857f7f457b403ab4db63599ae7c476a31dca563c
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-29 00:43:54 +0200

    add class Toy

--- classes/Food.php
+++ classes/Food.php
@@ -1,9 +1,8 @@
 <?php
     require_once __DIR__ . '/Product.php';
-
+    
     class Food extends Product {
-        public $expireDate = null;
-
+        public $expireDate;
 
         public function __construct(string $name, int $price, int $quantity, string $animalType, DateTime $expireDate) {
             parent::__construct($name, $price, $quantity, $animalType);
